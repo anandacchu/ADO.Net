7063dac5a139d8878b14373a64ae198119607878
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-14 12:08:14 +0530

    Inserting into two tables

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+create procedure spInsertIntoTwoTables
+(
+@Name varchar(120),
+@Gender varchar(120),
+@Address varchar(120),
+@EmployeeID int OUTPUT)
+as
+insert into  Employeetable (Name,Gender,Address)values(@Name,@Gender,@Address);
+SET @EmployeeID =SCOPE_IDENTITY();
